01a56e8a94de0e2af7b1f5ef16bff53da22d2d9d
taylor.ledingham <taylor.ledingham@gmail.com>
2014-11-13 16:13:06 -0800

    Initial Commit

---
+++ MusicTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MusicTest
+//
+//  Created by Taylor Ledingham on 2014-11-13.
+//  Copyright (c) 2014 Taylor Ledingham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
